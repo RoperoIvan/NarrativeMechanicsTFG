95f40aa3b6bb003e825030e7689ea968a309b1ba
RoperoIvan <ivan_ropero98@hotmail.com>
2021-03-29 18:42:01 +0200

    little fix to rotation

--- SubmarinePrototype/Assets/Scripts/MoveThroughRooms.cs
+++ SubmarinePrototype/Assets/Scripts/MoveThroughRooms.cs
@@ -1,36 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveThroughRooms : MonoBehaviour
 {
-    public float rotationSpeed = 0.1f;
+    public Button leftButton;
+    public Button rightButton;
+
+    public float rotationSpeed = 1f;
+
     public Transform[] looks;
+
     private int currentLook = 0;
+
     public void GoToRoom(bool isLeft)
     {
-        if(isLeft)
+        if(isLeft && currentLook > 0)
         {
+            currentLook--;
+            //if()
             //Camera.main.gameObject.transform.Rotate(Vector3.up, -90f);
-            StartCoroutine(ChangingRoom(currentLook--));
+            StartCoroutine(ChangingRoom(isLeft));
 
         }
-        else
+        else if(!isLeft && currentLook < 2)
         {
+            currentLook++;
             //Camera.main.gameObject.transform.Rotate(Vector3.up, 90f);
             //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, 0), rotationSpeed * Time.deltaTime);
-            StartCoroutine(ChangingRoom(currentLook++));
+            StartCoroutine(ChangingRoom(isLeft));
 
         }
     }
-     IEnumerator ChangingRoom(float angle)
+     IEnumerator ChangingRoom(bool isLeft)
     {
+        if(isLeft)
+        {
+            leftButton.interactable = false;
+        }
+        else
+        {
+            rightButton.interactable = false;
+        }
         float progress = 0.0f;
         while (progress < 1.0f)
         {
+            progress += (Time.deltaTime * rotationSpeed);
+            yield return 0;
             transform.rotation = Quaternion.Slerp(transform.rotation, looks[currentLook].rotation, progress);
-            yield return new WaitForEndOfFrame();
-            progress += Time.deltaTime * rotationSpeed;
+
+        }
+        if (isLeft)
+        {
+            leftButton.interactable = true;
+        }
+        else
+        {
+            rightButton.interactable = true;
         }
     }
 }
