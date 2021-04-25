fcdc632af4bcc6cd9e135aba5a801a4ba7527874
RoperoIvan <ivan_ropero98@hotmail.com>
2021-04-25 12:38:37 +0200

    get values from wheels

--- SubmarinePrototype/Assets/Scripts/Agents/PlayerController.cs
+++ SubmarinePrototype/Assets/Scripts/Agents/PlayerController.cs
@@ -20,51 +20,51 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            timeLineController.AddNewEvent(5f, TimeLineController.TimeEventType.BOMB);
-        }
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            timeLineController.AddNewEvent(60f, TimeLineController.TimeEventType.FREQUENCY);
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            timeLineController.AddNewEvent(20f, TimeLineController.TimeEventType.RADIO);
-        }
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            timeLineController.AddNewEvent(10f, TimeLineController.TimeEventType.VISUAL);
-        }
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    timeLineController.AddNewEvent(5f, TimeLineController.TimeEventType.BOMB);
+        //}
+        //if (Input.GetKeyDown(KeyCode.T))
+        //{
+        //    timeLineController.AddNewEvent(60f, TimeLineController.TimeEventType.FREQUENCY);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Y))
+        //{
+        //    timeLineController.AddNewEvent(20f, TimeLineController.TimeEventType.RADIO);
+        //}
+        //if (Input.GetKeyDown(KeyCode.U))
+        //{
+        //    timeLineController.AddNewEvent(10f, TimeLineController.TimeEventType.VISUAL);
+        //}
 
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            currentScreen = Screens.GLASS;
-            enemyFlagContainer.SetActive(true);
-            playerFlagContainer.SetActive(true);
-            UIVisualContainer.SetActive(true);
-            Debug.Log("IN VISUAL ROOM");
-        }
+        //if(Input.GetKeyDown(KeyCode.E))
+        //{
+        //    currentScreen = Screens.GLASS;
+        //    enemyFlagContainer.SetActive(true);
+        //    playerFlagContainer.SetActive(true);
+        //    UIVisualContainer.SetActive(true);
+        //    Debug.Log("IN VISUAL ROOM");
+        //}
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            GameManager.currentTension = Tension.PEACEFUL;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            GameManager.currentTension = Tension.LOW;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            GameManager.currentTension = Tension.MEDIUM;
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            GameManager.currentTension = Tension.DANGER;
-        }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            GameManager.currentTension = Tension.THREAT;
-        }
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    GameManager.currentTension = Tension.PEACEFUL;
+        //}
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    GameManager.currentTension = Tension.LOW;
+        //}
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    GameManager.currentTension = Tension.MEDIUM;
+        //}
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    GameManager.currentTension = Tension.DANGER;
+        //}
+        //if (Input.GetKeyDown(KeyCode.G))
+        //{
+        //    GameManager.currentTension = Tension.THREAT;
+        //}
     }
 }
--- SubmarinePrototype/Assets/Scripts/Communications/FrequencyCommunicationController.cs
+++ SubmarinePrototype/Assets/Scripts/Communications/FrequencyCommunicationController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using TMPro;
 
 public class FrequencyCommunicationController : MonoBehaviour
 {
-    public bool buttonPressed;
+    public float currentValue = 0f;
+    public float maxRotationValue = 340f;
+    public float minRotationValue = 0f;
+    public bool buttonPressed = false;
+    public TMP_Text value;
 
     private void Update()
     {
@@ -20,19 +24,27 @@ public class FrequencyCommunicationController : MonoBehaviour
         mousePosition.x - transform.position.x,
         mousePosition.y - transform.position.y
         );
+        
+       transform.up = direction;
+        currentValue = GetValueFromWheelRotation();
+        value.text = currentValue.ToString("F2");
+    }
 
-        transform.up = direction;
+    public float GetValueFromWheelRotation()
+    {
+        Vector3 eA = transform.rotation.eulerAngles;
+        float currentRotationAngle = eA.z;
+
+        return currentRotationAngle/maxRotationValue;
     }
 
     private void OnMouseDown()
     {
         buttonPressed = true;
-
     }
 
     private void OnMouseUp()
     {
         buttonPressed = false;
-
     }
 }
