f0658aa4bb4401d39738fea1fbb28cc761b343d9
RoperoIvan <ivan_ropero98@hotmail.com>
2021-05-05 17:33:46 +0200

    movement through rooms changed

--- SubmarinePrototype/Assets/Scripts/MoveThroughRooms.cs
+++ SubmarinePrototype/Assets/Scripts/MoveThroughRooms.cs
@@ -7,56 +7,85 @@ public class MoveThroughRooms : MonoBehaviour
 {
     public Button leftButton;
     public Button rightButton;
+    public GameObject commandContainer;
+    public GameObject shelvesContainer;
+    public GameObject engineContainer;
 
     public float rotationSpeed = 1f;
 
     public Transform[] looks;
 
     private int currentLook = 0;
-
+    private int currentRoom = 0;
+    private Screens[] rooms = { Screens.COMMAND, Screens.SHELVES, Screens.ENGINES };
     public void GoToRoom(bool isLeft)
     {
-        if(isLeft && currentLook > 0)
+        if(isLeft && currentRoom > 0)
         {
-            currentLook--;
+            currentRoom--;
+            ActivateRoom(rooms[currentRoom]);
             //if()
             //Camera.main.gameObject.transform.Rotate(Vector3.up, -90f);
-            StartCoroutine(ChangingRoom(isLeft));
+            //StartCoroutine(ChangingRoom(isLeft));
 
         }
-        else if(!isLeft && currentLook < 2)
+        else if(!isLeft && currentRoom < 2)
         {
-            currentLook++;
+            currentRoom++;
+            ActivateRoom(rooms[currentRoom]);
             //Camera.main.gameObject.transform.Rotate(Vector3.up, 90f);
             //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, 0), rotationSpeed * Time.deltaTime);
-            StartCoroutine(ChangingRoom(isLeft));
+            //StartCoroutine(ChangingRoom(isLeft));
         }
     }
-     IEnumerator ChangingRoom(bool isLeft)
-    {
-        if(isLeft)
-        {
-            leftButton.interactable = false;
-        }
-        else
-        {
-            rightButton.interactable = false;
-        }
-        float progress = 0.0f;
-        while (progress < 1.0f)
-        {
-            progress += (Time.deltaTime * rotationSpeed);
-            yield return 0;
-            transform.rotation = Quaternion.Slerp(transform.rotation, looks[currentLook].rotation, progress);
 
-        }
-        if (isLeft)
+    void ActivateRoom(Screens room)
+    {
+        switch(room)
         {
-            leftButton.interactable = true;
-        }
-        else
-        {
-            rightButton.interactable = true;
+            case Screens.COMMAND:
+                commandContainer.SetActive(true);
+                shelvesContainer.SetActive(false); ;
+                engineContainer.SetActive(false); ;
+                break;
+            case Screens.SHELVES:
+                commandContainer.SetActive(false); ;
+                shelvesContainer.SetActive(true);
+                engineContainer.SetActive(false); ;
+                break;
+            case Screens.ENGINES:
+                commandContainer.SetActive(false); ;
+                shelvesContainer.SetActive(false); ;
+                engineContainer.SetActive(true);
+                break;
         }
     }
+
+    // IEnumerator ChangingRoom(bool isLeft)
+    //{
+    //    if(isLeft)
+    //    {
+    //        leftButton.interactable = false;
+    //    }
+    //    else
+    //    {
+    //        rightButton.interactable = false;
+    //    }
+    //    float progress = 0.0f;
+    //    while (progress < 1.0f)
+    //    {
+    //        progress += (Time.deltaTime * rotationSpeed);
+    //        yield return 0;
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, looks[currentLook].rotation, progress);
+
+    //    }
+    //    if (isLeft)
+    //    {
+    //        leftButton.interactable = true;
+    //    }
+    //    else
+    //    {
+    //        rightButton.interactable = true;
+    //    }
+    //}
 }
